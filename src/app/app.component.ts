8643db531ae9eef6a042a01157fefb7c6bf3f64f
Mayurp375 <mayurpolojwar1234@gmail.com>
2023-07-13 16:11:42 +0530

    ability to To display Hello from BridgeLabz with a user inputted name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,8 @@ export class AppComponent {
   title = 'hellow world';
   imgUrl = "../assets/bl.jpg";
   url = "https://www.bridgelabz.com/";
-  userName:String = "";
+  userName: string = "";
+  nameError: string = "";
 
   ngOnInit(): void {
     this.title = "Hellow from Bridgelabz";
@@ -18,5 +19,18 @@ export class AppComponent {
   onClick(event: any): void {
     console.log("Save button is clicked!", event);
     window.open(this.url, "_blank");
+  }
+
+  onInput($event: any) {
+    console.log("Change Event Occured!", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$')
+
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    } else {
+      this.nameError = "Name is incorract";
+    }
+
   }
 }
